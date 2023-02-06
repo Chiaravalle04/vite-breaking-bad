7882df64faf2ffd0d08724acb1180c74238f6f3a
Chiaravalle04 <nicolas.chiaravalle@gmail.com>
2023-02-06 14:51:08 +0100

    add struttura base

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script>
+import axios from 'axios';
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue'
+import AppFooter from './components/AppFooter.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter
+  }
+}
+</script>
+
+<template>
+
+  <AppHeader/>
+  
+  <AppMain/>
+  
+  <AppFooter/>
+  
+</template>
+
+<style lang="scss">
+@import '../src/styles/main.scss';
+</style>
